20507ba4e551b8fb995b8096381a1e8418abaf0d
Zac <13089@holbertonstudents.com.au>
2026-09-09 15:21:45 +1000

    added task 6 code

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -46,6 +46,20 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+
+  return employee.workTeacherTasks();
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
