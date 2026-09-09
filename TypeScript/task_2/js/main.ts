e144904adedfec411507dcafca420727a00a829a
Zac <13089@holbertonstudents.com.au>
2026-09-09 15:24:36 +1000

    added task 7 code

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -58,8 +58,21 @@ function executeWork(employee: Director | Teacher): string {
   return employee.workTeacherTasks();
 }
 
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+
+  return 'Teaching History';
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
