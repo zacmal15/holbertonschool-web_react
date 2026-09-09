5d372425bedc937e9b921cb508811ce5d58ecd69
Zac <13089@holbertonstudents.com.au>
2026-09-09 15:01:07 +1000

    added third task

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -29,4 +29,15 @@ const director1: Directors = {
     numberOfReports: 17,
 };
 
-console.log(director1)
+console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+    firstName: string,
+    lastName: string,
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+console.log(printTeacher('John', 'Doe'));
