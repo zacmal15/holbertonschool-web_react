0d3e23dc40a43272213e8be72cc33334014d5b80
Zac <13089@holbertonstudents.com.au>
2026-09-09 15:10:27 +1000

    added task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -41,3 +41,33 @@ const printTeacher: printTeacherFunction = (
 ): string => `${firstName.charAt(0)}. ${lastName}`;
 
 console.log(printTeacher('John', 'Doe'));
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+const StudentClass: StudentClassConstructor = class StudentClass
+  implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+};
